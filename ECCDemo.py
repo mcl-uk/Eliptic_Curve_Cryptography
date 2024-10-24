04f68d8fe7d5c61cfa07fffeedad67b7934bf2f9
Steve Marchant <44552903+mcl-uk@users.noreply.github.com>
2024-10-24 16:23:29 +0100

    Update ECCDemo.py

--- ECCDemo.py
+++ ECCDemo.py
@@ -10,7 +10,7 @@ import os # for urandom numbers
 from hashlib import md5
 
 # By convention lower-case variables are integers, uppercase ones are (xy) points
-# Gobal curve paramters (public) as per brainpoolP192t1
+# Gobal curve paramters (public) as per brainpoolP192t1 from openSSL
 p  = 0xc302f41d932a36cda7a3463093d18db78fce476de1a86297 # prime modulus
 a  = 0xc302f41d932a36cda7a3463093d18db78fce476de1a86294 # curve a parameter
 b  = 0x13d56ffaec78681e68f9deb43b35bec2fb68542e27897b79 # curve b parameter, strangely not used
@@ -24,6 +24,14 @@ def inv(P):
     if xP == None: return [None, None]
     return [xP, -yP % p]
 
+def dbl(P):
+    if P[0] == None: return P
+    if P[1] == 0: return [None, None]
+    [xP, yP] = P
+    s = (3*pow(xP,2,p)+a) * pow(2*yP, -1, p)
+    xR = (pow(s,2,p) - 2*xP) % p
+    return [ xR, (-yP + s*(xP-xR)) % p ]
+
 def add(P,Q):
     if P == Q: return dbl(P)
     if P[0] == None: return Q
@@ -35,17 +43,9 @@ def add(P,Q):
     xR = (pow(s,2,p) - xP -xQ) % p
     return [ xR, (-yP + s*(xP-xR)) % p ]
 
-def dbl(P):
-    if P[0] == None: return P
-    if P[1] == 0: return [None, None]
-    [xP, yP] = P
-    s = (3*pow(xP,2,p)+a) * pow(2*yP, -1, p)
-    xR = (pow(s,2,p) - 2*xP) % p
-    return [ xR, (-yP + s*(xP-xR)) % p ]
-
 # Here we 'multiply' P, an xy point on the curve, by integer k within our
 # finite number space. Think of it as moving along the curve by a distance k 
-# This operation is the heart and soul of ECC crypto.
+# This operation is the heart and soul of eliptic curve cryptography.
 def mul(P, k):
     if P[0] == None: return P
     N, R = P, [None, None]
@@ -87,16 +87,16 @@ print(f"CypherText:       {'/'.join(str(x) for x in R)}")
 Srx = mul(R, da) # (4) re-create the shared secret xy point S
 assert Srx == S
 
-# This works because, in ECC world:
+# This works because (see mul() above), in ECC world:
 # If G.da (1) takes you to point Qa and Qa.r (2) takes you on to point S, then similarly
 # G.r (3) takes you to point R and R.da (4) takes you to [Ta-Da!] S.
 #   G -------------da-----------> Qa -----r-----> S
 #   G -----r-----> R  -------------da-----------> S
 # Both Alice and Bob have essentially moved the same distance along the curve and arrived
-# at the same point because [r + da] == [da + r]. The same journey, different way-points.
+# at the same point because [r + da] == [da + r]. Same journey, different way-points.
 # In ECC it's easy to find a destination point given a starting point and a distance but
-# very dificult to find the distance between two points, this is the source of the
-# asymetry and why it all works.
+# very dificult to find the distance between two arbitrary points, this is the source of
+# the asymetry and why it all works. It is both amazing and great!
 
 # Now re-create Alice's secret AES key
 srx = md5(str(Srx).encode()).digest() # byte string
